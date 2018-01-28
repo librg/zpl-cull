d33c51a86ca7016b5a21cb0888ee01d474f4d3a0
Dominik Madarász <zaklaus@outlook.com>
2018-01-28 11:14:42 +0100

    Update zpl_cull.h

--- include/zpl_cull.h
+++ include/zpl_cull.h
@@ -2,14 +2,6 @@
 
 ZPL - Tree culler module
 
-License:
-    This software is dual-licensed to the public domain and under the following
-    license: you are granted a perpetual, irrevocable license to copy, modify,
-    publish, and distribute this file as you see fit.
-
-Warranty:
-    No warranty is implied, use at your own risk.
-
 Usage:
     #define ZPLC_IMPLEMENTATION exactly in ONE source file right BEFORE including the library, like:
 
